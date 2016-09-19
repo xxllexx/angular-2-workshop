f7d05ddef21cc68a1013d86cbac00ea3040dac68
alexk wix <alexk@wix.com>
2016-09-19 20:13:09 +0300

    let`s add some styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Ng Forms Builder</h1>`
+  template: `
+  	<h1>Ng Forms Builder</h1>
+  	<div class="wrapper">
+  		<input type="text" placeholder="your text"/>
+  		<select>
+  			<option>Test</option>
+  		</select>
+  		<input type="radio" />
+  		<input type="checkbox" />
+  		<textarea></textarea>
+  	</div>
+  `
 })
 
 export class AppComponent {
