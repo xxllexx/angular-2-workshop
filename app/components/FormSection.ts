c6fda80212ef11792980ba42cfc4440908d56f42
alexk wix <alexk@wix.com>
2016-09-20 17:01:13 +0300

    add missed component

---
+++ app/components/FormSection.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'form-section',
+	template: `
+		<section>
+			<div>
+				<input type="text" placeholder="your text"/>
+		  		<select>
+		  			<option>Single Answer</option>
+		  			<option>Multiple Answer</option>
+		  			<option>Own Answer</option>
+		  		</select>
+			</div>
+			<ul class="options">
+				<li>
+					<input type="radio" disabled/>
+					<input type="text" placeholder="option text"/>
+					<button>x</button>
+				</li>
+			</ul>
+			<div>
+				<a href="#">+ add option</a>
+			</div>
+
+		</section>
+		<button>Remove</button>
+	`
+})
+
+export default class FormSection{
+	@Input()
+	sectionSettings: Section
+}
