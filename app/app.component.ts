ab0ebd2a2f9e73b93c94769984df7fc2656ab7e2
alexk wix <alexk@wix.com>
2016-09-23 12:06:24 +0300

    asddasdasdasads

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { TodoService } from './todoService';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Ng Forms Builder</h1>`
+  template: `
+  	<h1>Ng Forms Builder!!!!</h1>
+  	<div><input type="text" #box (keyup.enter)="add(box.value); box.value=''"/></div>
+  	<ul>
+
+  		<todo *ngFor="let todo of getTodos()" [todo]="todo" (onDone)="toggleCompleted($event)"></todo>
+  	</ul>
+  `,
+  providers: [TodoService]
 })
 
 export class AppComponent {
-	
+	constructor(private todos: TodoService){}
+	getTodos(){
+		return this.todos.getTodos();
+	}
+	add(val){
+		this.todos.addTodo(val);
+	}
+	toggleCompleted(todo){
+		todo.toggleCompleted();
+	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule }      from '@angular/core';
+import { NgModule}      from '@angular/core';
+import { FormsModule}      from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 
+import TodoComp from './TodoComp';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, TodoComp],
   bootstrap:    [ AppComponent ]
 })
 
